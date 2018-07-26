958c3fc55c1f17f1b951526831fa40bc306531bc
Thomas <tom.n.knol@gmail.com>
2018-07-26 09:12:05 -0500

    add and edit a recipe

--- src/test/java/tnk/recipes/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/tnk/recipes/repositories/UnitOfMeasureRepositoryIT.java
@@ -14,7 +14,7 @@ import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-public class UnitOfMeasureRepositoryTests {
+public class UnitOfMeasureRepositoryIT {
 
     @Autowired
     UnitOfMeasureRepository unitOfMeasureRepository;
